fc36c1ea94e11635a93a3b43a8b76433d97a76a4
alsails <mash.pogonina2014@yandex.ru>
2025-03-28 22:42:54 +0300

    Задание GUI 3.5 пункт 1

---
+++ src/gui3_5_1/Click.java
@@ -0,0 +1,25 @@
+package gui3_5_1;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+class Click {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Нажми пробел");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setFocusable(true);
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    JOptionPane.showMessageDialog(frame, "Мария", "Информация", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
+
